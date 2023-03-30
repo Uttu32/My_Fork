74742c291de0305735941a2abce6d0b234f01f6f
Uttu32 <utkarshksharm7878@gmail.com>
2023-03-30 10:12:46 +0530

    Guess The number game and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar.jsx';
+import Home from './Components/Home';
+import About from './Components/About';
+import Game from './Components/Game';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home />}>
+          {' '}
+        </Route>
+
+        <Route path="/about" element={<About />}>
+          {' '}
+        </Route>
+        
+        <Route path="/game" element={<Game />}>
+          {' '}
+        </Route>
+        <Route path="/login" element={<Login />}>
+          {' '}
+        </Route>
+        <Route path="/register" element={<Register />}>
+          {' '}
+        </Route>
+      </Routes>
+      {/* </Navbar> */}
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.Module.css';
+
+function Navbar() {
+  return (
+    <div className="container">
+      <nav>
+        <ul className="navbar">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li>
+            <NavLink to="/game">Game</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
