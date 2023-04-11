9fad622ea656c15fad8c78e6683d947e405249b0
SamadSRahman <samadsrahman@gmail.com>
2023-04-11 15:21:22 +0530

    Pushing after corrections

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const url = "https://jsonplaceholder.typicode.com/posts"
-  const [users, setUsers] = useState([])
-
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios.get(url).then((response) => {
       setUsers(response.data);
-      console.log(users)
     });
   }, []);
 
-  const removeUser=(index)=> {
-    users.splice(index, 1)
-  setUsers([...users]  )
-  }
+  const removeUser = (index) => {
+    users.splice(index, 1);
+    setUsers([...users]);
+  };
   return (
     <div className="App">
       <h1> Data</h1>
       <table>
         <thead>
           <tr>
-          <th >S. No</th>
-          <th>Title</th>
-          <th>Body</th>
+            <th>S. No</th>
+            <th>Title</th>
+            <th>Body</th>
             <th>Action</th>
-            </tr>
+          </tr>
         </thead>
         <tbody>
-          {
-            users.map((ele, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{ele.title}</td>
-                <td>{ele.body}</td>
-                <td><button onClick={()=>removeUser(index)}>❌</button></td>
-                </tr>
-            ))
-          }
+          {users.map((ele, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{ele.title}</td>
+              <td>{ele.body}</td>
+              <td>
+                <button onClick={() => removeUser(index)}>❌</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-     
     </div>
   );
 }
